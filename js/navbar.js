4103d9494c0e57ee90884261de2290d9cff4e88a
unknown <ivan.nikic@gmail.com>
2020-08-11 16:21:28 +0200

    commit

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+// NAVIGATION SCRIPT
+function toggleNavbar(collapseID) {
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("block");
+  };
